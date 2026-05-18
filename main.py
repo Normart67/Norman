1d6acef3d8a7b6e177f799ed4afd6172e35479bc
loyalshift <administrative@loyalshift.com>
2026-05-17 19:02:14 -0600

    feat: our first canvas

---
+++ main.py
@@ -0,0 +1,10 @@
+import turtle
+
+turtle.setup(800, 600)  # Explicit window size
+turtle.bgcolor("lightblue")
+turtle.shape("turtle")
+turtle.color("black")
+turtle.pensize(3)
+turtle.forward(100)
+
+turtle.done()  # Keeps window open
